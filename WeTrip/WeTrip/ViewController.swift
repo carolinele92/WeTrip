d4cdd7eae3ff396f1b1e3c1bffe79df54f778349
Long Nguyen <lnguyen215@csc.com>
2017-03-14 15:48:27 +0700

    init project

---
+++ WeTrip/WeTrip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeTrip
+//
+//  Created by Nguyen Nam Long on 3/14/17.
+//  Copyright © 2017 Nguyen Nam Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
